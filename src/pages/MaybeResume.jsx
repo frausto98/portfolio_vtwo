27a70f0cd81965fb51c2f90e4cb8860bfbdfab3d
frausto98 <frausto9298@yahoo.com>
2023-09-02 01:50:26 -0700

    adjusted resume page just need to  import. everything else looks smooth

--- src/pages/MaybeResume.jsx
+++ src/pages/MaybeResume.jsx
@@ -1,10 +1,10 @@
-import { Box, Block, Image, Columns} from "react-bulma-components";
+import { Box, Block, Image, Columns } from "react-bulma-components";
 
 const styles = {
     mainContainer: {
         background: 'DodgerBlue',
         padding: '20px',
-        
+
     },
     title: {
         background: 'DodgerBlue',
@@ -28,6 +28,18 @@ const styles = {
         borderStyle: 'solid',
         borderWidth: '2px',
     },
+    paragraph: {
+        fontSize: '17px',
+        textAlign: 'center',
+        color: 'white',
+        margin: 'auto',
+        background: 'DodgerBlue',
+        borderRadius: 10,
+        borderColor: 'white',
+        borderStyle: 'solid',
+        borderWidth: '1px',
+        padding: '10px',
+    }
 
 }
 
@@ -38,16 +50,27 @@ function MaybeResume() {
                 <Columns>
                     <Columns.Column size={4} offset={4}>
                         <h1 style={styles.title}>
-                            This is my Resume!
+                            This is to Contact Me!
                         </h1>
                     </Columns.Column>
                 </Columns>
 
                 <Columns>
-                    <Columns.Column size={6} offset={3}>
-                        <h1 style={styles.text}>
-                            Here, I will display my resume.. in  the future!
-                        </h1>
+                    <Columns.Column size={4} offset={4}>
+                        <div>
+                            <Block >
+                                <p style={styles.paragraph}>
+                                    Below is my resume as it currently stands. I have to adjust it to  reflect  the new skills I've developed for the  class.
+                                </p>
+                            </Block>
+                            <Block>
+                                <Image
+                                    size='128x256'
+                                    src=''
+                                />
+                            </Block>
+
+                        </div>
                     </Columns.Column>
                 </Columns>
             </div>
